42a7f744e5a9cb0fa493f8049fd69a9c3c62c5a0
ydc-k <1032massanb@gmail.com>
2025-08-26 02:46:33 +0900

    Add files via upload

---
+++ clear.js
@@ -0,0 +1,16 @@
+const checkbox = document.getElementById('three');
+const button = document.getElementById('send');
+const info = document.getElementById('info');
+const menu = document.getElementById('drawer_input');
+
+var url = "https://ja.wikipedia.org/wiki/%E5%A4%A7%E4%B8%8D%E6%AD%A3%E8%A7%A3"
+
+button.addEventListener('click', () => {
+
+location.href = "7kmodcr.htm"
+
+});
+
+
+
+
---
+++ nazoEX.js
@@ -0,0 +1,36 @@
+const checkbox = document.getElementById('three');
+const button = document.getElementById('send');
+const info = document.getElementById('info');
+const menu = document.getElementById('drawer_input');
+
+var url = "https://ja.wikipedia.org/wiki/%E5%A4%A7%E4%B8%8D%E6%AD%A3%E8%A7%A3"
+
+button.addEventListener('click', () => {
+
+if (one.checked && three.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (two.checked && three.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (four.checked && three.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (five.checked && three.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (six.checked && three.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (seven.checked && three.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (eight.checked && three.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (nine.checked && three.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (three.checked) {
+location.href = "cleardlkci.htm"
+} else {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+}
+
+});
+
+
+
+
