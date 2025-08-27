a9e1f3617e92dc838c7dc1eea2f483c8d42cb9d4
ydc-k <1032massanb@gmail.com>
2025-08-27 22:44:05 +0900

    Add files via upload

---
+++ nazo4.js
@@ -0,0 +1,35 @@
+const checkbox = document.getElementById('three');
+const button = document.getElementById('send');
+const info = document.getElementById('info');
+const menu = document.getElementById('drawer_input');
+
+var url = "https://ja.wikipedia.org/wiki/%E5%A4%A7%E4%B8%8D%E6%AD%A3%E8%A7%A3"
+
+button.addEventListener('click', () => {
+
+if (eight.checked && nine.checked && two.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (eight.checked && nine.checked && three.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (eight.checked && nine.checked && four.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (eight.checked && nine.checked && five.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (eight.checked && nine.checked && seven.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (eight.checked && nine.checked && one.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+} else if (eight.checked && nine.checked && two.checked) {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+}  else if (eight.checked && nine.checked) {
+location.href = "5rvscnr.htm"
+} else {
+alert("‚Ç‚¤‚â‚çŠÔˆá‚Á‚Ä‚¢‚é‚æ‚¤‚¾");
+}
+
+});
+
+
+
+
+
