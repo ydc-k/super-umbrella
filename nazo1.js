e9494a51ee1c279fafc0a26141c994b65813a70b
ydc-k <1032massanb@gmail.com>
2025-08-29 23:59:16 +0900

    complete

--- nazo1.js
+++ nazo1.js
@@ -8,25 +8,25 @@ var url = "https://ja.wikipedia.org/wiki/%E5%A4%A7%E4%B8%8D%E6%AD%A3%E8%A7%A3"
 button.addEventListener('click', () => {
 
 if (one.checked && three.checked) {
-alert("�ǂ����Ԉ���Ă���悤��");
+alert("どうやら間違っているようだ");
 } else if (two.checked && three.checked) {
-alert("�ǂ����Ԉ���Ă���悤��");
+alert("どうやら間違っているようだ");
 } else if (four.checked && three.checked) {
-alert("�ǂ����Ԉ���Ă���悤��");
+alert("どうやら間違っているようだ");
 } else if (five.checked && three.checked) {
-alert("�ǂ����Ԉ���Ă���悤��");
+alert("どうやら間違っているようだ");
 } else if (six.checked && three.checked) {
-alert("�ǂ����Ԉ���Ă���悤��");
+alert("どうやら間違っているようだ");
 } else if (seven.checked && three.checked) {
-alert("�ǂ����Ԉ���Ă���悤��");
+alert("どうやら間違っているようだ");
 } else if (eight.checked && three.checked) {
-alert("�ǂ����Ԉ���Ă���悤��");
+alert("どうやら間違っているようだ");
 } else if (nine.checked && three.checked) {
-alert("�ǂ����Ԉ���Ă���悤��");
+alert("どうやら間違っているようだ");
 } else if (three.checked) {
 location.href = "2akskazk.htm"
 } else {
-alert("�ǂ����Ԉ���Ă���悤��");
+alert("どうやら間違っているようだ");
 }
 
 });
