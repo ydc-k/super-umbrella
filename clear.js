5d50be823f9ca9d98e8adf4efaf356c48be8335d
ydc-k <1032massanb@gmail.com>
2025-09-01 20:22:53 +0900

    Add files via upload

--- clear.js
+++ clear.js
@@ -1,13 +1,21 @@
-const checkbox = document.getElementById('three');
-const button = document.getElementById('send');
-const info = document.getElementById('info');
-const menu = document.getElementById('drawer_input');
+const button = document.getElementById('explay');
+const tweet = document.getElementById('tweet');
+const ex = document.getElementById('ex');
 
-var url = "https://ja.wikipedia.org/wiki/%E5%A4%A7%E4%B8%8D%E6%AD%A3%E8%A7%A3"
+var url = "https://x.com/home"
 
 button.addEventListener('click', () => {
 
-location.href = "7kmodcr.htm"
+ex.classList.add('is-active');
+setTimeout(() => {
+location.href = '7kmodcr.htm';
+}, 2000)
+
+});
+
+tweet.addEventListener('click', () => {
+
+location.href = 'https://x.com/home';
 
 });
 
--- clearex.js
+++ clearex.js
@@ -1,15 +1,20 @@
 const checkbox = document.getElementById('three');
-const button = document.getElementById('send');
-const info = document.getElementById('info');
-const menu = document.getElementById('drawer_input');
+const button = document.getElementById('tweet');
+const finish = document.getElementById('finish');
 
-var url = "https://ja.wikipedia.org/wiki/%E5%A4%A7%E4%B8%8D%E6%AD%A3%E8%A7%A3"
 
 button.addEventListener('click', () => {
 
 location.href = "https://x.com/home"
 
 });
+
+finish.addEventListener('click', () => {
+
+location.href = "https://ja.wikipedia.org/wiki/%E3%82%A2%E3%83%B3%E3%82%B1%E3%83%BC%E3%83%88"
+
+});
+
 
 
 
